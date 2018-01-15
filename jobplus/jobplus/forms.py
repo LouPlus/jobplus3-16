cfafc81a0976e41e35d5e50ff4d52b035eb7a1db
wmingjun94 <wmingjun94@gmail.com>
2018-01-15 11:30:43 +0800

    Fix issue 9

--- jobplus/jobplus/forms.py
+++ jobplus/jobplus/forms.py
@@ -1,9 +1,12 @@
 from flask_wtf import FlaskForm
 from wtforms import StringField, PasswordField, SubmitField, BooleanField,SelectField
 from wtforms.validators import Length, Email, EqualTo, Required
+from jobplus.models import db, User, Company
+from wtforms import ValidationError
 
 
 
+PEOPLE_CHOICES = [('1','1-50'),('2','51-100'),('3','101-500'),('4','>500')]
 class Base(FlaskForm):
     __abstract__ = True
     username = StringField('Username', validators=[Required(), Length(3, 24)])
@@ -11,15 +14,43 @@ class Base(FlaskForm):
     password = PasswordField('Password', validators=[Required(), Length(6, 24)])
     repeat_password = PasswordField('Password again', validators=[Required(), EqualTo('password')])
 
+    def validate_username(self, field):
+        if User.query.filter_by(username=field.data).first():
+            raise ValidationError("User already exists")
+    def validate_email(self, field):
+        if User.query.filter_by(email=field.data).first():
+            raise ValidationError('Email already registered')
+
 class CompanyRegisterForm(Base):
 
-    companyName = StringField('CompanyName', validators=[Required(), Length(1,100)])
+    companyName = StringField('Company Name', validators=[Required(), Length(1,100)])
     location = StringField('Location', validators=[Required()])
-    number_of_people = SelectField('Number of People', choices = [('1','1-50'),('2','51-100'),('3','101-500'),('4','>500')])
+    number_of_people = SelectField('Number of People', choices = PEOPLE_CHOICES)
     submit = SubmitField('Submit')
+
+    def create_companyProfile(self):
+        user = User()
+        company = Company()
+        company.location = self.location.data
+        company.number_of_people = dict(PEOPLE_CHOICES).get(self.number_of_people.data)
+
+        company.save()
+        user.company_id = company.id
+        user.username = self.username.data
+        user.email = self.email.data
+        user.password = self.password.data
+        user.realName = self.companyName.data
+        user.save()
 
 class UserRegisterForm(Base):
     submit = SubmitField('Submit')
+
+    def create_user(self):
+        user = User()
+        user.username = self.username.data
+        user.email = self.email.data
+        user.password = self.password.data
+        user.save()
 
 class LoginForm(FlaskForm):
     email = StringField('Email', validators=[Required(), Email()])
--- jobplus/jobplus/handlers/front.py
+++ jobplus/jobplus/handlers/front.py
@@ -1,6 +1,8 @@
-from flask import Blueprint, render_template
-from jobplus.models import Job
+from flask import Blueprint, render_template, redirect, url_for
+from jobplus.models import Job,User
 from jobplus.forms import LoginForm, UserRegisterForm, CompanyRegisterForm
+from flask import flash
+from flask_login import login_user, logout_user, login_required
 
 front = Blueprint('front', __name__)
 
@@ -9,17 +11,36 @@ def index():
     jobs = Job.query.all()
     return render_template('index.html', jobs=jobs)
 
-@front.route('/companyRegister')
+@front.route('/companyRegister', methods=['GET', 'POST'])
 def company_register():
-	form = CompanyRegisterForm()
-	return render_template('company_register.html', form = form)
+    form = CompanyRegisterForm()
+    if form.validate_on_submit():
+        form.create_companyProfile()
+        flash("Success!", 'success')
+        return redirect(url_for('.login'))
+    return render_template('company_register.html', form = form)
 
-@front.route('/userRegister')
+@front.route('/userRegister', methods=['GET', 'POST'])
 def user_register():
-	form = UserRegisterForm()
-	return render_template('user_register.html', form  = form)
+    form = UserRegisterForm()
+    if form.validate_on_submit():
+        form.create_user()
+        flash("Success!", 'success')
+        return redirect(url_for('.login'))
+    return render_template('user_register.html', form  = form)
 
-@front.route('/login')
+@front.route('/login', methods=['GET', 'POST'])
 def login():
-	form = LoginForm()
-	return render_template('login.html', form = form)
+    form = LoginForm()
+    if form.validate_on_submit():
+        user = User.query.filter_by(email=form.email.data).first()
+        login_user(user, form.remember_me.data)
+        return redirect(url_for('.index'))
+    return render_template('login.html', form = form)
+
+@front.route('/logout', methods=['GET','POST'])
+@login_required
+def logout():
+    logout_user()
+    flash("You have successfull logged out")
+    return redirect(url_for('.index'))
